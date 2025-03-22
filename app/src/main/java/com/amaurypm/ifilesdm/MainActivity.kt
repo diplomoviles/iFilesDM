c6778d488da33fd4c7af023e41a39c75c8c19770
Amaury PM <amaury.pema@gmail.com>
2025-03-22 10:36:08 -0600

    Usando XML

--- app/src/main/java/com/amaurypm/ifilesdm/MainActivity.kt
+++ app/src/main/java/com/amaurypm/ifilesdm/MainActivity.kt
@@ -6,16 +6,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import com.amaurypm.ifilesdm.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import org.simpleframework.xml.core.Persister
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
+import java.io.StringWriter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var serializer: Persister
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Instanciamos el serializer
+        serializer = Persister()
 
         //toast("hola")
         //toast("Amaury", Toast.LENGTH_LONG)
@@ -49,7 +57,22 @@ class MainActivity : AppCompatActivity() {
             if(binding.tietText.text.toString().isNotEmpty()){
 
                 //Pasamos la cadena a un arreglo de bytes
-                val bytesToSave = binding.tietText.text.toString().encodeToByteArray()
+                //val bytesToSave = binding.tietText.text.toString().encodeToByteArray()
+
+                val student = Student(name = binding.tietText.text.toString())
+
+                //Creamos un escritor para el xml
+                val writer = StringWriter()
+
+                //Serializamos el objeto en xml
+                serializer.write(student, writer)
+
+                //Obtenemos el xml como cadena
+                val xmlString = writer.toString()
+
+                val bytesToSave = xmlString.encodeToByteArray()
+
+
 
                 try{
 
@@ -114,7 +137,12 @@ class MainActivity : AppCompatActivity() {
                     binding.tvContent.text = content.decodeToString()
                     fis.close()*/
 
-                    binding.tvContent.text = file.readText()
+                    //binding.tvContent.text = file.readText()
+
+
+                    val xmlString = file.readText()
+                    val student = serializer.read(Student::class.java, xmlString)
+                    binding.tvContent.text = "Nombre: ${student.name}\nId: ${student.id}"
 
                 }else{
                     sbMessage(
---
+++ app/src/main/java/com/amaurypm/ifilesdm/Student.kt
@@ -0,0 +1,12 @@
+package com.amaurypm.ifilesdm
+
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.Root
+
+@Root(name = "student")
+data class Student(
+    @field:Element(name = "id")
+    var id: Long = System.currentTimeMillis(),
+    @field:Element(name = "name")
+    var name: String? = null
+)
